b44796eaad68b8bc1a86c4a0386ec9ebe1cdca38
Chris Haynes <chris@lamplightdev.com>
2021-07-10 23:18:08 +0100

    Update comments

--- src/wafer-mixin.js
+++ src/wafer-mixin.js
@@ -99,8 +99,8 @@ export const WaferMixin = (superclass) =>
       this._updatePromise = null;
 
       /**
-       * Map of all properties to their values, that have changed in the current
-       * update cycle
+       * Map of all properties to their new values, that have changed in the
+       * current update cycle
        *
        * @private
        * @readonly
@@ -305,7 +305,9 @@ export const WaferMixin = (superclass) =>
     }
 
     /**
-     * Update property targets for all properties that are marked as changed
+     * Update property targets for all properties whose values have changed or
+     * have been manually requested
+     *
      *
      * @param {String[] | null} [props]
      * List of property names whose targets should be updated, even if
@@ -321,49 +323,143 @@ export const WaferMixin = (superclass) =>
      * @returns {void}
      */
     update(props = []) {
+      /**
+       * List of properties that have been manually requested to be updated
+       * irrespective of whether their values have changed
+       *
+       * @type {string[]}
+       */
       const propNames = props ? (props.length ? props : this._propNames) : [];
 
+      /**
+       * Promise that resolves when all targets in this cycle have been
+       * applied
+       **/
       this._updatePromise =
         this._updatePromise ||
         Promise.resolve().then(async () => {
+          /**
+           * Map of property names whose value has changed (or was manually
+           * requested) to their new (existing) values
+           *
+           * @type {Map<string, any>}
+           */
           const allToUpdate = new Map();
+
+          /**
+           * Object of property name/values containing all the properties and
+           * their values at the point the update was initiated
+           *
+           * @type {Object<string, any>}
+           */
           const initialValues = {
             ...this._props,
           };
 
           /**
+           * Object of property names whose targets should *not* be updated
+           *
            * @type {Object<string, boolean>}
            */
-          let noUpdate = {};
+          const noUpdate = {};
 
+          /**
+           * Add all manually requested property names to the `_toUpdate` map
+           */
           if (propNames.length) {
+            /**
+             * indicate that there are changes to process - `_newChanges` would
+             * be `false` at this point if there are only manual changes to
+             * process, so flip it to true
+             */
             this._newChanges = true;
+
             for (const propName of propNames) {
+              /**
+               * Only add the prop if it's not already there - don't want to
+               * clobber any existing new value
+               */
               if (!this._toUpdate.has(propName)) {
+                /**
+                 * Set the value in the map to the existing value, which for
+                 * a manual update is the 'new' value
+                 */
                 this._toUpdate.set(propName, this[propName]);
               }
             }
           }
 
+          /**
+           * Continue to process changes until there are no new changes
+           */
           while (this._newChanges) {
+            /**
+             * Map of property names whose values have changed to their
+             * old values
+             *
+             * @type {Map<string, any>}
+             */
             const changed = new Map();
 
+            /**
+             * Cycle through all properties that have had their values changed
+             * or who have been manually requested
+             */
             for (const [name, value] of this._toUpdate.entries()) {
               if (this._props[name] === value && !propNames.includes(name)) {
+                /**
+                 * Remove any properties whose new value is now equal to the old
+                 * value as long as they haven't been manually requested
+                 */
                 allToUpdate.delete(name);
                 changed.delete(name);
               } else {
+                /**
+                 * Add the property and it's new value to the list of properties
+                 * whose targets should be updated
+                 */
                 allToUpdate.set(name, value);
+                /**
+                 * Add the property and it's old (existing) value to the list
+                 * of properties that have changed
+                 */
                 changed.set(name, this._props[name]);
+
+                /**
+                 * Update the current value of the property to the new value
+                 */
                 this._props[name] = value;
               }
             }
 
+            /**
+             * Indicate all changed have been processed. Note: this may be
+             * flipped back to true if a property is updated in the `changed`
+             * callback
+             */
             this._newChanges = false;
+
+            /**
+             * Clear the update map
+             */
             this._toUpdate.clear();
 
             if (changed.size > 0) {
+              /**
+               * List of properties whose targets should not be updated
+               * (as returned from the changed callback)
+               *
+               * @type {string[]}
+               */
               const doNotUpdate = (await this.changed(changed)) || [];
+
+              /**
+               * Include the properties in `doNotUpdate` in the definitive
+               * list of properties whose targets should not be updated, but
+               * remove properties from the same list that where included
+               * previously but have since changed (and have not had their
+               * name returned from the `changed` callback again)
+               */
               for (const name of changed.keys()) {
                 if (doNotUpdate.includes(name)) {
                   noUpdate[name] = true;
@@ -374,55 +470,139 @@ export const WaferMixin = (superclass) =>
             }
           }
 
+          /**
+           * Map of property names whose targets will be updated to their
+           * old values
+           *
+           * @type {Map<string, any>}
+           */
           const updated = new Map();
 
+          /**
+           * For all properties whose targets should be updated (due to value
+           * changes, or manual request)
+           */
           for (const [name] of allToUpdate.entries()) {
+            /**
+             * The new property value
+             *
+             * @type {any}
+             */
             const value = this[name];
 
+            /**
+             * For all properties whose value has changed or been manually
+             * requested
+             */
             if (initialValues[name] !== value || propNames.includes(name)) {
+              /**
+               * Update the attribute from the new property value
+               */
               this._setFromProp(name, value);
 
+              /**
+               * Add the property and old value to list of properties whose
+               * targets should be updated, unless it has been marked otherwise
+               */
               if (!noUpdate[name]) {
                 updated.set(name, initialValues[name]);
               }
             }
           }
 
+          /**
+           * Indicate the current changes have all been processed
+           */
           this._changePromise = null;
 
           if (updated.size > 0) {
+            /**
+             * Don't apply updates if the element was rendered on the server
+             * and this is the rehydration update
+             */
             if (!this.serverRendered()) {
+              /**
+               * List of properties whose targets should be updated
+               *
+               * @type {string[]}
+               */
               const updatedProps = [...updated.keys()];
 
-              /** @type {Object<string, boolean>} */
+              /**
+               * Object of other properties whose targets should be updated
+               * due to being defined as being triggered by another component
+               * change
+               *
+               * @type {Object<string, boolean>}
+               */
               const otherTriggers = {};
 
               for (const name of updatedProps) {
-                const { triggers = [] } = this.props[name];
+                const {
+                  /**
+                   * Properties that are listed as being triggered by this
+                   * property
+                   *
+                   * @type {string[]}
+                   */
+                  triggers = [],
+                } = this.props[name];
+
+                /**
+                 * Update all targets of this property. The result is `await`ed
+                 * for when used in the server context where updates need to be
+                 * applied before being sent in the response
+                 */
                 await this.updateTargets(name);
 
+                /**
+                 * Update master list of other properties whose targets should
+                 * be updated
+                 *
+                 */
                 for (const trigger of triggers) {
                   otherTriggers[trigger] = true;
                 }
               }
 
+              /**
+               * Update the properties that were listed as triggers of
+               * changed properties
+               */
               for (const name of Object.keys(otherTriggers)) {
+                /**
+                 * Don't run the updates if they have already been run
+                 */
                 if (!updatedProps.includes(name)) {
                   await this.updateTargets(name);
                 }
               }
             }
 
+            /**
+             * Indicate the current updates have all been processed
+             */
             this._updatePromise = null;
 
             if (this._firstUpdate) {
+              this._firstUpdate = false;
               await this.firstUpdated(updated);
             }
 
+            /**
+             * The response is `await`ed so that in the server context if
+             * further work is initiated that results in property changes
+             * (and targets updated), the response shouldn't be set until all
+             * changes have been processed
+             */
             await this.updated(updated);
-            this._firstUpdate = false;
           } else {
+            /**
+             * Indicate the current updates have all been processed, and
+             * the next update will not be the first
+             */
             this._updatePromise = null;
+            this._firstUpdate = false;
           }
         });
     }
@@ -439,7 +619,8 @@ export const WaferMixin = (superclass) =>
      *
      * @returns {string[]|Promise<string[]>|void}
      * If an array of property names (or a Promise that resolves to an array) is
-     * returned then those properties will not have their targets updated.
+     * returned then those properties will not have their targets updated. If
+     * a promise is returned then it will be `await`ed
      *
      */
     // eslint-disable-next-line no-unused-vars
@@ -452,9 +633,13 @@ export const WaferMixin = (superclass) =>
      *
      * @param {Map<String, any>} updated
      * A Map of properties and their old values that have had their targets
-     * updated in the current cycle
+     * updated in the current cycle.
      *
      * @returns {void|Promise<void>}
+     * If a promise is returned then it will be
+     * `await`ed - this is useful in a server context where, if further work is
+     * initiated that results in property changes (and targets updated), the
+     * response shouldn't be set until all changes have been processed.
      */
     // eslint-disable-next-line no-unused-vars
     updated(updated) {}
